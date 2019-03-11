ab0b5830c6a0208b184e05611ffae5e58cfe92b4
Foo@Win <jf_mejiar547@live.com>
2019-03-10 21:29:35 -0600

    respaldo inicial, query de respaldo

--- querys/backuping.sql
+++ querys/backuping.sql
@@ -1,4 +1,5 @@
---restaurar bd luego de hacer git pull para obtener los ultimos cambios
+--restaurar bd luego de hacer git pull para obtener los cambios mas recientes
+restore headeronly from disk='ruta';
 restore filelistonly from disk='ruta';
 restore database NorthwindCO from disk='ruta'
 with move 'archivo mdf' to 'ruta_mdf',
@@ -7,6 +8,6 @@ replace;
 
 --por favor añadir nombre y descripcion al hacer respaldo luego de modificar la bd
 backup database NorthwindCO
-to disk='D:\Repos\Visual Studio Source\northwind-finance\restore_me.bak'
-with name='Inicial',
-description='Respaldo incial limpio';
+to disk='ruta'
+with name='nombre',
+description='descripcion';
