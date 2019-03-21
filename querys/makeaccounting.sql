885ef7905581ee5afa383c59525dddeace92d79c
Foo@Win <jf_mejiar547@live.com>
2019-03-21 14:55:15 -0600

    pend testing & sp

--- querys/makeaccounting.sql
+++ querys/makeaccounting.sql
@@ -38,11 +38,46 @@ ADD CONSTRAINT [CK_account_nat] check (nat in('D', 'A'));
 ALTER TABLE Accounts
 ADD CONSTRAINT [CK_account_finstat] check (finance_stat in('BG', 'ER'));
 
+/*La tabla reg accounts registra el valor en libro de las cuentas en un
+determinado año, con el fin de obtener el estado financiero respectivo de ese año.
+Por convencion, las fechas de ese año son el ultimo dia*/
 
 CREATE TABLE Reg_Accounts(
 	id_reg INTEGER PRIMARY KEY IDENTITY(1,1),
 	id_account INTEGER FOREIGN KEY REFERENCES Accounts(id_account),
 	book_value MONEY,
-	yr DATE,
+	yr DATE
 );
 
+CREATE TABLE F_Act (
+	ac_cod INTEGER PRIMARY KEY,
+	descr NVARCHAR(60),
+	dept NVARCHAR(5),
+	book_value MONEY
+);
+
+ALTER TABLE F_Act
+ADD CONSTRAINT [CK_f_act_dept] check (dept in('SALES', 'ADMIN'));
+
+CREATE TABLE Purchases(
+	id_purc INTEGER PRIMARY KEY IDENTITY(1,1),
+	product_id INTEGER FOREIGN KEY REFERENCES Products(ProductID),
+	UnitQty INT,
+	UnitCost MONEY,
+	purchase_date DATE
+);
+/*To test*/
+
+
+CREATE TABLE Transactions (
+	id_transact INTEGER PRIMARY KEY IDENTITY(1,1),
+	descr NVARCHAR(70),
+	transact_date DATE
+);
+
+CREATE TABLE Transact_details(
+	id_transact_details INTEGER PRIMARY KEY IDENTITY(1,1),
+	id_accounts INTEGER FOREIGN KEY REFERENCES Accounts(id_account) NOT NULL,
+	id_transact INTEGER FOREIGN KEY REFERENCES Transactions(id_transact) NOT NULL,
+	change_amount MONEY NOT NULL
+)
